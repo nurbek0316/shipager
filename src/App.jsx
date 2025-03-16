4ed167e531d05983f800c1960a61fa6f5047f01f
nurbek0316 <maulethan.nm@gmail.com>
2025-03-16 22:22:39 +0500

    Your commit message here

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
+import Doctors from "./pages/Doctors";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+import MyProfile from "./pages/MyProfile";
+import MyAppointments from "./pages/MyAppointments";
+import Appointment from "./pages/Appointment";
+import NavBar from "./components/NavBar";
+
+const App = () => {
+  return (
+    <div className="mx-4 sm:mx-[10%]">
+      <NavBar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/doctors" element={<Doctors />} />
+        <Route path="/doctors/:speciality" element={<Doctors />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/my-profile" element={<MyProfile />} />
+        <Route path="/my-appointments" element={<MyAppointments />} />
+        <Route path="/appoinment/:docId" element={<Appointment />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Header";
+import SpecialutyMenu from "../components/SpecialutyMenu";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <SpecialutyMenu />
+    </div>
+  );
+};
+
+export default Home;
