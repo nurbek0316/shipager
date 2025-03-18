6ffd277ebac6b6bcafbe24eebf68043dc0f37f81
nurbek0316 <maulethan.nm@gmail.com>
2025-03-18 19:03:03 +0500

    1.0.0

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,11 +3,16 @@ import { assets } from "../assets/assets";
 import { NavLink, useNavigate } from "react-router-dom";
 const NavBar = () => {
   const navigate = useNavigate();
-  const [showMenu, setShowMenu] = useState(false);
-  const [token, setToken] = useState(false);
+  const [showMenu, setShowMenu] = useState(true);
+  const [token, setToken] = useState(true);
   return (
     <div className="flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400">
-      <img className="w-44 cursor-pointer" src={assets.logo} alt="" />
+      <img
+        onClick={() => navigate("/")}
+        className="w-44 cursor-pointer"
+        src={assets.logo}
+        alt=""
+      />
       <ul className="hidden md:flex items-start gap-5 font-medium">
         <NavLink to={"/"}>
           <li className="py-1">HOME</li>
@@ -31,7 +36,7 @@ const NavBar = () => {
       </ul>
       <div className="flex items-center gap-4">
         {token ? (
-          <div className="flex items-center gap-2 cursor-pointer group relative">
+          <div className="flex items-center gap-2 cursor-pointer group relative ">
             <img className="w-8 rounded-full" src={assets.profile_pic} alt="" />
             <img className="w-2.5" src={assets.dropdown_icon} alt="" />
             <div className="absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block">
@@ -42,11 +47,18 @@ const NavBar = () => {
                 >
                   My Profile
                 </p>
-                <p onClick={() => navigate("/my-appoinments")} 
-                className="hover:text-black cursor-pointer">
+                <p
+                  onClick={() => navigate("/my-appoinments")}
+                  className="hover:text-black cursor-pointer"
+                >
                   My Appoinments
                 </p>
-                <p onClick={() => setToken(false)}className="hover:text-black cursor-pointer">Logout</p>
+                <p
+                  onClick={() => setToken(false)}
+                  className="hover:text-black cursor-pointer"
+                >
+                  Logout
+                </p>
               </div>
             </div>
           </div>
